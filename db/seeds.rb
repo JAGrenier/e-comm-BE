e3bc412ffdb3b79d75fcc08d59cf794361dd9ae8
github username <github email address>
2020-10-21 11:41:00 -0600

    added tester seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+
+    Project.create(
+        organization: "org1", 
+        url: "org1.com",
+        summary: "org1 is about things", 
+        donation_options: "How to help org1"
+    )
